109554f2ef4a6faeff451e6903d94b25c4703239
Frontend JavaScript <frontendjavascript@C3-03.local>
2023-03-28 20:47:52 +0300

    kommentos

---
+++ curs1/demo.js
@@ -0,0 +1,50 @@
+class Carte {
+    static reducere = 10;
+    mypret;
+    constructor(titlu,autor, editura,an,pret) {
+        this.titlu = titlu;
+        this.autor = autor;
+        this.editura = editura;
+        this.an = an;
+        this.pret =pret;
+    }
+
+    get pret(){
+        console.log(`Sunt in getter`);
+        return this.mypret;
+    }
+
+    set pret(pretNou){
+
+        console.log(`Sunt in setter`);
+        if(pretNou <= 0){
+            throw 'Pretul nu poate fi actualizat';
+        }
+        else{
+            this.mypret = pretNou;
+        }
+        this.mypret = pretNou;
+
+    }
+
+    info(){
+       console.log(`Titlul este ${this.titlu}, autor ${this.autor}`);
+    }
+
+    deschidCartea(){
+        console.log('Am deschis cartea la pagina 15');
+    }
+    randomIntFromInterval(min, max) { // min and max included 
+       return Math.floor(Math.random() * (max - min + 1) + min)
+     }
+     pretNou(){
+        this.pret = this.pret -(Carte.reducere * this.pret) / 100
+     }
+     modificaPret(diferenta){
+        this.pret = this.pret - diferenta;
+     }
+
+}
+
+// Curs 2 Module
+// export { Carte }
